310621ee43f1e44110b72d013b9544028987d07c
Cameobc <dkqtk@naver.com>
2022-06-07 21:59:28 +0900

    MVC 2강 - 요청데이터까지

--- src/main/java/hello/sevlet/ServletApplication.java
+++ src/main/java/hello/sevlet/ServletApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @ServletComponentScan // 서블릿 자동 등록 -> 현재 패키지/하위 패키지를 다 뒤져서 서블릿을 찾아서 등록하고 실행하도록 도움
 @SpringBootApplication
-public class SevletApplication {
+public class ServletApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SevletApplication.class, args);
+		SpringApplication.run(ServletApplication.class, args);
 	}
 
 }
--- src/test/java/hello/sevlet/ServletApplicationTests.java
+++ src/test/java/hello/sevlet/ServletApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SevletApplicationTests {
+class ServletApplicationTests {
 
 	@Test
 	void contextLoads() {
